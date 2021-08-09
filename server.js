0d26b992f78f58b68827456ca5e61dc96e0d0eef
Smaily <b.smaily95@gmail.com>
2021-08-10 00:15:12 +0100

    Change server file

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser');
+;
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+// app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -22,3 +30,13 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+app.post('/api/shorturl', (req,res)=>{
+  var url = req.body
+
+  console.log(url)
+
+  res.json({"original_url": url});
+
+} )
