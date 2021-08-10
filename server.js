2a7656c59e3d11ee57925ef3f72fbfabe3afb130
Smaily <b.smaily95@gmail.com>
2021-08-10 14:38:09 +0100

    DB changes

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-;
+const validUrl = require('valid-url');
+
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect( process.env.DB_URI , {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+console.log("connected")); 
+
+console.log(mongoose.connection.readyState)
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -14,9 +21,8 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
+// app.urlencoded()
 
-// parse application/json
-// app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -31,12 +37,51 @@ app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
 
+// const schema = new mongoose.Schema({ name: 'string', id: inrger });
+// const Tank = mongoose.model('Tank', schema);
+
+// const Schema = mongoose.Schema
+// const URLSchema = new Schema({
+// oldURL : String,
+// id : Number
+// })
+
+// const Model = mongoose.model
+// const URL = Model('Urls',URLSchema)
+
+// const NodeJsGuide = new URL({name : 'Googl.com' })
+
+
 
 app.post('/api/shorturl', (req,res)=>{
-  var url = req.body
+  const url = req.body.url
 
   console.log(url)
 
-  res.json({"original_url": url});
+  if (validUrl.isUri(url)){
 
+              
+      const urlSchema = new mongoose.Schema({
+        oldURL: String,
+        id: Number,
+        newURL: String
+      });
+
+      const URI = mongoose.model("URI", urlSchema);
+
+      const dbUrl = new URI({
+        oldURL: url,
+        id: 1,
+        newURL: "12345"
+      });
+
+      dbUrl.save();
+
+
+    res.json({"original_url": url ,"short_url": "id"});;
+} 
+else {
+    res.json({"error":"Invalid URL"});
+}
+  
 } )
